aab2cc21e52dcda719315f9f659eb89e5e82bb3e
David Bach <david.bach21@gmail.com>
2019-07-01 10:33:54 +0200

    Add emacs skim customization

---
+++ emacs/.emacs.d/customizations/skim.el
@@ -0,0 +1,17 @@
+;; Some functions to access notes from skim
+
+(defun save-skim-notes (pdf)
+	"save notes as .txt file into temp directory"
+	(call-process "/Applications/Skim.app/Contents/SharedSupport/skimnotes" nil 0 nil "get" "-format" "text" pdf "/tmp/temp_skim_notes.txt"))
+
+(defun read-skim-notes ()
+	"read the latest temp skim file"
+	(shell-command-to-string "cat /tmp/temp_skim_notes.txt"))
+
+(defun counsel-skim-notes ()
+	"show notes content in counsel."
+	(interactive)
+	(let* ((notes (read-skim-notes)))
+		(ivy-read "%d Choose note:  " notes 
+							:action #'read-skim-notes
+							:require-match t)))
