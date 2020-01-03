8d2d82a921e539f53067ed56b516763abde2cade
David Bach <david.bach21@gmail.com>
2020-01-03 09:49:08 +0100

    Add custom faces to julia jupyter repl

--- emacs/.emacs.d/customizations/julia.el
+++ emacs/.emacs.d/customizations/julia.el
@@ -11,3 +11,10 @@
 
 (add-hook 'jupyter-repl-mode-hook
           (lambda () (local-set-key (kbd "C-c M-o") 'jupyter-repl-clear-cells)))
+
+;; Jupyter Repl customizations
+(custom-set-faces
+ '(jupyter-repl-input-prompt ((t (:foreground "dark blue"))))
+ '(jupyter-repl-output-prompt ((t (:foreground "dark red"))))
+ '(jupyter-repl-traceback ((t (:background "firebrick3")))))
+
