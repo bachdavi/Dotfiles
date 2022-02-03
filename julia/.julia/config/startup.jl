6cbd381edde47bd8b59e85f99eddb9809d0f5806
David Bach <david@DavidBook.local>
2022-02-03 09:44:21 +0100

    Update julia startup

--- julia/.julia/config/startup.jl
+++ julia/.julia/config/startup.jl
@@ -1,24 +1,5 @@
-function installed()
-    deps = Pkg.dependencies()
-    installs = Dict{String, VersionNumber}()
-    for (uuid, dep) in deps
-        dep.is_direct_dep || continue
-        dep.version === nothing && continue
-        installs[dep.name] = dep.version
-    end
-    return installs
-end
-
-
-atreplinit() do repl
-    try
-        @eval using Pkg
-        haskey(installed(), "Revise") || @eval Pkg.add("Revise")
-    catch
-    end
-    try
-        @eval using Revise
-        @async Revise.wait_steal_repl_backend()
-    catch
-    end
+try
+    using Revise
+catch e
+    @warn "Error initializing Revise" exception=(e, catch_backtrace())
 end
